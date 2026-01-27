83fde5f073f5891e80d9e382449dcdc2f837204b
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-01-27 13:19:10 +0600

    practice few topic

---
+++ 0 CPH academy/Modul 01/Contest - 1 Data types, Variables, Operators/practice.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void solve()
+{
+    cout << "Hello, World!" << endl;
+}
+       
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
